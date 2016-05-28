eb24c656b8b537c8aab9811b211488e2ff1ee360
Alp Eren Can <alperenc@gmail.com>
2016-05-28 16:10:48 +0300

    add 4sq api client and update .gitignore

---
+++ Propty/FoursquareClient.swift
@@ -0,0 +1,13 @@
+//
+//  FoursquareClient.swift
+//  Propty
+//
+//  Created by Alp Eren Can on 28/05/16.
+//  Copyright © 2016 Alp Eren Can. All rights reserved.
+//
+
+import UIKit
+
+class FoursquareClient: NSObject {
+
+}
---
+++ Propty/FoursquareConstants.swift
@@ -0,0 +1,69 @@
+//
+//  FoursquareConstants.swift
+//  Propty
+//
+//  Created by Alp Eren Can on 28/05/16.
+//  Copyright © 2016 Alp Eren Can. All rights reserved.
+//
+
+import Foundation
+
+extension FoursquareClient {
+    
+    // MARK: Constants
+    struct Constants {
+        
+        // MARK: Client ID & Secret
+        static let ClientID = FoursquareKeys.ClientID
+        static let ClientSecret = FoursquareKeys.ClientSecret
+        
+        // MARK: URLs
+        static let BaseURL = "https://api.foursquare.com/v2/"
+        
+        // MARK: Parameter Constants
+        static let CategoryID = "4e67e38e036454776db1fb3a"
+        static let Intent = "browse"
+        static let Radius = 1000
+    }
+    
+    // MARK: Methods
+    struct Methods {
+        
+        // Venue Search
+        static let Search = "venues/search"
+    }
+    
+    // MARK: Parameter Keys
+    struct ParameterKeys {
+        
+        static let ClientID = "client_id"
+        static let ClientSecret = "client_secret"
+        static let LatLong = "ll"
+        static let CategoryID = "category_id"
+        static let Intent = "intent"
+        static let Radius = "radius"
+        
+    }
+    
+    // MARK: JSON Response Keys
+    struct ResponseKeys {
+        
+        // Response
+        static let Response = "response"
+        
+        // Venues
+        static let Venues = "venues"
+        
+        // Venue
+        static let ID = "id"
+        static let Name = "name"
+        static let Location = "location"
+        
+        // Location
+        static let Lat = "lat"
+        static let Long = "lng"
+        static let Distance = "distance"
+        static let FormattedAddress = "formattedAddress"
+        
+    }
+}
